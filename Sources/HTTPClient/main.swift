8d911a98695fb46493281a44b24f925cbac9bf80
Joe Smith <yasumoto7@gmail.com>
2019-04-22 12:21:47 -0700

    Hints for HTTPClient

--- Sources/HTTPClient/main.swift
+++ Sources/HTTPClient/main.swift
@@ -3,4 +3,26 @@
 /// This exercise allows students the opportunity to work with the `NIOHTTP1`
 /// toolset, and send requests (typically locally, using something like
 /// `cak/server.swift`) to a server that does not use TLS.
-/// 
+///
+
+// We need both NIO and NIOHTTP1
+
+// We need an EventLoopGroup
+
+// We need to Bootstrap a Client
+// And initialize its channel
+// Then add HTTP Client Handlers to the pipeline
+
+// Once that's done, we need to connect to localhost on port 8000
+
+// We should make sure we've actually connected by using the Future
+
+
+// We need to create an HTTPRequestHead
+
+// Then wrap it inside HTTPClientRequestPart, and wrap that in NIOAny
+
+// Then we can write that to the channel
+
+// Lastly, we should wrap an `end` HTTPClientRequestPart in NIOAny
+// and then wait for the completion
